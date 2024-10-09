2b956927fc4279d9feb3e83219107827a14c7492
hpl_work <peilin.huo_work@qq.com>
2024-10-09 22:16:32 +0800

    Initial commit

--- app/src/main/java/com/hpl/my_music/MainActivity.java
+++ app/src/main/java/com/hpl/my_music/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+//这是测试
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
