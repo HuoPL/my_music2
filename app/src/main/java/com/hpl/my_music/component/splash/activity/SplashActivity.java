839037fe7270b6fe154445ab2dc64765099e0f25
hpl_work <peilin.huo_work@qq.com>
2024-10-15 18:26:24 +0800

    Initial commit

---
+++ app/src/main/java/com/hpl/my_music/component/splash/activity/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.hpl.my_music.component.splash.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.hpl.my_music.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
