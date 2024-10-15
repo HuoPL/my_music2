4853459649605e1fb68c7130df8c97d0b9980bd8
hpl_work <peilin.huo_work@qq.com>
2024-10-15 20:15:34 +0800

    Initial commit

--- app/src/main/java/com/hpl/my_music/component/splash/activity/SplashActivity.java
+++ app/src/main/java/com/hpl/my_music/component/splash/activity/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.hpl.my_music.component.splash.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.hpl.my_music.R;
+import com.hpl.my_music.util.SuperDateUtil;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.qmuiteam.qmui.util.QMUIWindowInsetHelper;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -17,7 +21,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+        //设置出沉浸式状态栏
 
-
+        QMUIStatusBarHelper.translucent(this);
+//        QMUIWindowInsetHelper.setStatuBarLightMode
+        QMUIStatusBarHelper.setStatusBarLightMode(this);
+        //设置版本的年份
+        //获取年份
+        int year= SuperDateUtil.currentYear();
+        TextView copyrightView = findViewById(R.id.copyright);
+        copyrightView.setText(getResources().getString(R.string.copyright,year));
     }
+
 }
